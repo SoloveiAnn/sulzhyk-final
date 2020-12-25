e912f83b1f3d81712bba214aeab6e10ecd19c8e4
SoloveiAnn <soloveianna8@gmail.com>
2020-12-25 09:51:21 +0000

    test

--- js/quiz.js
+++ js/quiz.js
@@ -6,12 +6,16 @@ class Quiz {
                 this.questions = questions;
                 this.showQuestion();
             })
-            document.querySelector('.check')
+            document.querySelector('.check').addEventListener('clock', this.checkAnswer);
     }
     showQuestion() {
         const idx = Math.floor(Math.random() * this.questions.length);
         this.currQuestion = this.questions[idx];
         document.querySelector('.question').value = this.currQuestion.q;
+    }
+    checkAnswer() {
+         const answer = document.querySelector('.answer').value;
+         
     }
 }
 new Quiz ();
